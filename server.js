e2e8360680864c014e82fa8b1d498dfc4a9d1f81
SaadBadr <saadbadr23@gmail.com>
2021-04-26 02:28:29 +0200

    serving files

--- server.js
+++ server.js
@@ -1,14 +1,49 @@
 const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
-const hostname = '127.0.0.1'
-const port = '1337'
+const mimeTypes = {
+    'html': 'text/html',
+    'css': 'text/css',
+    'jpeg': 'image/jpeg',
+    'jpg': 'image/jpg',
+    'png': 'image/png',
+    'js': 'text/javascript',
+}
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World!\n')
-})
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-})
+http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const fileName = path.join(process.cwd(), url.pathname)
+    console.log(`Loading ${url}`)
+    var stats
+   
+    try {
+        stats = fs.lstatSync(fileName)
+    } catch (error) {
+        res.statusCode = 404
+        res.setHeader('Content-type', 'text/plain')
+        res.write('404 Not Found\n')
+        res.end()
+        return
+    }
+
+    if(stats.isFile()) {
+        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]]
+        res.statusCode = 200
+        res.setHeader('Content-type', mimeType)
+        const fileStream = fs.createReadStream(fileName)
+        fileStream.pipe(res)
+    }
+    else if(stats.isDirectory()) {
+        res.statusCode = 302
+        res.setHeader('Location', 'index.html')
+        res.end()
+    }
+    else{
+        res.statusCode = 500
+        res.setHeader('Content-type', 'text/plain')
+        res.write('500 Internal Error\n')
+        res.end()
+    }
+}).listen(1337)
